4f900c0497b3182e21424fefb161d509a2f9df4f
Thiago Veronezi <thiago@veronezi.org>
2020-09-21 11:30:37 -0400

    solutions 1

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -4,13 +4,11 @@
 // freshly created vector from fill_vec to its caller.
 // Execute `rustlings hint move_semantics4` for hints!
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
-
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+    let mut vec1 = fill_vec(&vec0);
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
     vec1.push(88);
@@ -19,8 +17,8 @@ fn main() {
 }
 
 // `fill_vec()` no longer take `vec: Vec<i32>` as argument
-fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.clone();
 
     vec.push(22);
     vec.push(44);
